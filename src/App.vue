903ae2be409fe618c32279317e4da8854593ca95
Kamil Wyremski <kamil.wyremski@gmail.com>
2023-07-06 10:07:07 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Lorem ipsum generator</h1>
+    <button v-for="i in 5" :key="i">
+      {{ i }} paragraphs
+    </button>
+  </main>
+</template>
+
+<style scoped>
+h1{
+  font-weight: 500;
+  font-size: 2.6rem;
+}
+</style>
